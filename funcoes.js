14641dc94b6ae930d9a6909f11a803b3d8587cce
Renato Dias <natovirtual@gmail.com>
2023-08-27 14:42:47 -0300

    Adicionado arquivos com a resolução dos exercícios de funções em JS

---
+++ funcoes.js
@@ -0,0 +1,105 @@
+// 1
+// const numero = prompt(parseFloat('Digite um número: '))
+
+// function imprimeNumero (numero){
+//   alert(numero)
+// }
+
+// imprimeNumero(numero)
+
+// 2
+// const n1 = parseInt(prompt('Digite o primeiro número: '))
+// const n2 = parseInt(prompt('Digite o segundo número: '))
+// const n3 = parseInt(prompt('Digite o terceiro número: '))
+
+// function soma (n1, n2, n3){
+// let resultadoSoma = n1 + n2 + n3
+// return resultadoSoma
+// }
+
+// alert(soma(n1, n2, n3))
+
+// 3
+// const nota1 = parseFloat(prompt('Digite primeira nota: '))
+// const nota2 = parseFloat(prompt('Digite segunda nota: '))
+
+
+// function calculaMedia (nota1, nota2){
+// let media = (nota1 + nota2) /12
+// return media.toFixed(2)
+// }
+
+// alert(`A média anual é ${calculaMedia(nota1, nota2)}`)
+
+// 4
+// let n1 = parseInt(prompt('Por favor, digite o primeiro número: '))
+// let n2 = parseInt(prompt('Por favor, digite o segundo número: '))
+
+// function calculaResto(n1, n2){
+//   let restoDivisao = n1 % n2
+//   if(restoDivisao === 0){
+//   alert(`Esta é uma divisão exata e o resto é ${restoDivisao}`)
+// }else{
+//   alert(`Esta é uma divisão inexata e o resto é ${restoDivisao}`)
+// }
+// } 
+
+// calculaResto(n1, n2)
+
+// 5
+// const numero = parseInt(prompt('Digite um numero: '))
+// function calculaQuadrado (numero){
+//   let quadrado = (numero * numero)
+//   return quadrado
+// }
+
+// alert(`O quadrado do número digitado é ${calculaQuadrado(numero)}`)
+
+// 6
+// let nota1 = parseFloat(prompt('Por favor, digite o primeira nota: '))
+// let nota2 = parseFloat(prompt('Por favor, digite o segunda nota: '))
+// let nota3 = parseFloat(prompt('Por favor, digite o terceira nota: '))
+// let nota4 = parseFloat(prompt('Por favor, digite o quarta nota '))
+
+// function calculaMedia(n1, n2, n3, n4){
+//   let mp = ((n1*1) + (n2*2) + (n3*3) + (n4*4)) / (10)
+//   return mp.toFixed(2)
+  
+// }
+
+// alert(`A média ponderada é ${calculaMedia(nota1, nota2, nota3, nota4)}`)
+
+// 7
+// let notaMatematica = parseFloat(prompt('Por favor, digite a nota de Matemática: '))
+// let notaPortugues = parseFloat(prompt('Por favor, digite a nota de Português: '))
+// let notaCG = parseFloat(prompt('Por favor, digite a nota de Conhecimentos Gerais: '))
+// let notaInformatica = parseFloat(prompt('Por favor, digite a nota de Informática '))
+
+// function calculaMedia(n1, n2, n3, n4){
+//   let mp = ((n1*2) + n2 + n3 + n4) / (5)
+//   return mp.toFixed(2)
+  
+// }
+
+// alert(`A média ponderada é de: ${calculaMedia(notaMatematica, notaPortugues, notaCG, notaInformatica)}`)
+
+// 8
+// let valorSalario = parseFloat(prompt('Por favor, insira o valor do salário a ser calculado: '))
+
+// function calculaReajuste(valorSalario){
+//   valorSalario = valorSalario + (valorSalario * 0.03)
+//   return valorSalario.toFixed(2)
+// }
+
+// alert(`O valor do salário reajustado é ${calculaReajuste(valorSalario)}`)
+
+// 9
+// let base = parseFloat(prompt('Informe a base do triângulo: '))
+// let altura = parseFloat(prompt('Informe a altura do triângulo: '))
+
+// function calculaArea(b, h){
+//   let area = (b * h) / 2
+//   return area.toFixed(2)
+// }
+
+// alert(`A área do triângulo é ${calculaArea(base, altura)}`)
